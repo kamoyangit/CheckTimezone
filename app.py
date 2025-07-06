8a34aa69216d1e9975091e48a595b316e135d4d8
kamoyan <kamoyan55@jcom.zaq.ne.jp>
2025-07-06 22:42:16 +0900

    Update check

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
 import streamlit as st
 from zoneinfo import ZoneInfo
 import datetime
+import os
+
+# --- デバッグフラグ ---
+# ここをTrue/Falseで切り替える
+DEBUG = os.environ.get('DEBUG_OPTION')
+# --------------------
+
+# --- ヘルパー関数 ---
+def is_lunch_time():
+    # ローカルだと上手くいくが、Streamlit.ioにデプロイするとNG
+    # jst = pytz.timezone('Asia/Tokyo')
+    # now_jst = datetime.datetime.now(jst).time()
+    # zoneifoを使う方法
+    jst = ZoneInfo("Asia/Tokyo")
+    now_jst = datetime.datetime.now(jst).time()
+    start_time = datetime.time(11, 0)
+    end_time = datetime.time(14, 0)
+    if DEBUG:
+        return True
+    else :
+        return start_time <= now_jst < end_time
+
 
 jst = ZoneInfo("Asia/Tokyo")
 now_jst = datetime.datetime.now(jst)
 st.write(f"現在の日本時間: {now_jst}")
+
+if is_lunch_time():
+    st.write(f"ランチタイムだよ")
+else:
+    st.wirte(f"ランチタイムじゃないよ")
